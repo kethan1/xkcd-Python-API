443fb77094e59a905efd93d6d64af45e7e437112
Kethan Vegunta <kethan@vegunta.com>
2021-05-31 18:33:46 -0700

    add files

---
+++ xkcd_api.py
@@ -0,0 +1,20 @@
+import requests
+import random
+
+def get_latest_comic():
+    response = requests.get("https://xkcd.com/info.0.json")
+    return response.json()
+
+def download_image(json_content: dict):
+    img = requests.get(json_content["img"])
+    with open("comic.png", "wb") as comicbook:
+        comicbook.write(img.content)
+
+def get_comic(number: int):
+    random_comic = requests.get(f"https://xkcd.com/{number}/info.0.json")
+    return random_comic.json()
+
+def get_random_comic():
+    latest_comic_num = get_latest_comic()["num"]
+    random_comic = requests.get(f"https://xkcd.com/{random.randint(1, latest_comic_num)}/info.0.json")
+    return random_comic.json()
