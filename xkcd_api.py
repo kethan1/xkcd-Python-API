b2d6cae02aee74eb827e9fd0bf99d559f1844a05
Kethan Vegunta <kethan@vegunta.com>
2021-06-04 08:47:50 -0700

    add more type annotations

--- xkcd_api.py
+++ xkcd_api.py
@@ -1,20 +1,20 @@
 import requests
 import random
 
-def get_latest_comic():
+def get_latest_comic() -> dict:
     response = requests.get("https://xkcd.com/info.0.json")
     return response.json()
 
-def download_image(json_content: dict):
+def download_image(json_content: dict) -> None:
     img = requests.get(json_content["img"])
     with open("comic.png", "wb") as comicbook:
         comicbook.write(img.content)
 
-def get_comic(number: int):
+def get_comic(number: int) -> dict:
     random_comic = requests.get(f"https://xkcd.com/{number}/info.0.json")
     return random_comic.json()
 
-def get_random_comic():
+def get_random_comic() -> dict:
     latest_comic_num = get_latest_comic()["num"]
     random_comic = requests.get(f"https://xkcd.com/{random.randint(1, latest_comic_num)}/info.0.json")
     return random_comic.json()
